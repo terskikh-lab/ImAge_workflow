9e885c10fff2a183dc78e6f5319c5ebeec355d07
KNinomiya <knt.eat.24splash@gmail.com>
2025-06-19 10:40:07 +0800

    Refactor ezload and ezsave functions to simplify file handling and remove unused code

--- workflows/subfunctions/ezsave.py
+++ workflows/subfunctions/ezsave.py
@@ -6,14 +6,8 @@
 
 #import modules=======================
 import pickle
-import h5py
 #=====================================
 # @jit(nopython=True)
 def ezsave(vList,file):
     with open(file, 'wb') as f:
         pickle.dump(vList, f)
-        
-def ezsave_hdf5(vList,file):
-    with h5py.File(file, 'w') as f:
-        for key in vList.keys():
-            f.create_dataset(key, data=vList[key])
